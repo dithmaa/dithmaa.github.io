1f3ed999001f1b60689754d394d91d9508a4e299
dithmaa <36035289+dithmaa@users.noreply.github.com>
2019-05-09 15:55:16 +0800

    Адаптив

--- mercury/js/main.js
+++ mercury/js/main.js
@@ -23,3 +23,48 @@ $(".features").slick({
     }
   ] 
 });
+$('.dragdrop-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 600,
+  slideToShow: 1,
+  slideToScroll: 1,
+  prevArrow: '<button type="button" class="dragdrop-prev dragdrop-arrow"><img alt="prev" src="../img/dragdrop/prev.png"></button>',
+  nextArrow: '<button type="button" class="dragdrop-next dragdrop-arrow"><img alt="next" src="../img/dragdrop/next.png"></button>'
+});
+//drag'n drop
+const fill = document.querySelector('.task');
+const empties = document.querySelectorAll('.drop');
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for(const empty of empties){
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+//Drag Functions
+function dragStart(){
+  this.className += ' hold';
+  setTimeout( () => this.className = 'invisible', 0); 
+} 
+function dragEnd(){
+  this.className = "task";
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered'; 
+} 
+function dragLeave() {
+  this.className = 'drop';
+}
+function dragDrop() {
+this.className = 'drop';
+this.append(fill);
+}
