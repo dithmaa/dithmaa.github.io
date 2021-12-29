518443128bb8efd04b90f0d1c54e517ae20a364a
Номто Балтуков <nomto_04@mail.ru>
2021-12-29 23:12:20 +0800

    Add files via upload

---
+++ yanazambalova/js/main.js
@@ -0,0 +1,24 @@
+const 
+    hamburgerButton = document.querySelector('.hamburger'),
+    topNav = document.querySelector('.top-nav'),
+    menuMobile = document.querySelector('.menu-mobile');
+
+hamburgerButton.addEventListener('click', ()=>{
+    hamburgerButton.classList.toggle('is-active');
+    menuMobile.classList.toggle('active');
+	topNav.classList.toggle('active');
+})
+
+
+$(".menu, .menu-mobile").on("click","a", function (event) {
+	//отменяем стандартную обработку нажатия по ссылке
+	event.preventDefault();
+	//забираем идентификатор бока с атрибута href
+	let id = $(this).attr('href'),
+
+	//узнаем высоту от начала страницы до блока на который ссылается якорь
+		top = $(id).offset().top;
+	
+	//анимируем переход на расстояние - top за 1500 мс
+	$('body,html').animate({scrollTop: top}, 1000);
+});
